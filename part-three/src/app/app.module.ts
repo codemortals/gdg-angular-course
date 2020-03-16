b35fc04a4d7447483644ac39ffc834af87d97b61
Eddie Jaoude <eddie@jaoudestudios.com>
2020-03-16 20:21:47 +0000

    Form with search
    Co-authored-by: Andrew Cunliffe <andrew@sysen.co.uk>

--- part-three/src/app/app.module.ts
+++ part-three/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ToggleButtonComponent } from './toggle-button/toggle-button.component';
 import { HomeComponent } from './home/home.component';
@@ -24,6 +26,8 @@ import { ContactComponent } from './contact/contact.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatListModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- part-three/src/app/home/home.component.ts
+++ part-three/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public addForm: FormGroup;
+  public searchForm: FormGroup;
+
+  public data = [
+    'Eddie',
+    'Andrew',
+    'ClamyLee',
+    'Gsuss',
+    'CodeMortals',
+    'Reshma Patel',
+    'N Nas,'
+  ];
+  public filteredData: Array<string>;
+
+  constructor(
+    private forms: FormBuilder,
+  ) { }
 
   ngOnInit() {
+
+    this.addForm = this.forms.group({
+      name: []
+    });
+
+    this.searchForm = this.forms.group({
+      search: [],
+    });
+
+    this.filteredData = this.data;
+
+    this.searchForm.valueChanges.subscribe(() => this.performSearch());
+
   }
 
+  public performSearch(): void {
+    const check = new RegExp(this.searchForm.get('search').value, 'i');
+    this.filteredData = this.data.filter((name) => check.test(name));
+  }
+
+  public addName(): void {
+    this.data = [this.addForm.get('name').value, ...this.data];
+    this.performSearch();
+  }
 }
