748e985b26ca47330e2ea980f2b2c219f97e72da
Eddie Jaoude <eddie@jaoudestudios.com>
2020-03-16 19:33:39 +0000

    home and contact page

--- part-three/src/app/app.module.ts
+++ part-three/src/app/app.module.ts
@@ -6,14 +6,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { ToggleButtonComponent } from './toggle-button/toggle-button.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToggleButtonComponent
+    ToggleButtonComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
+    MatToolbarModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
